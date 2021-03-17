c1505ed5a15c461371c26db39f801bd5b4b92cd1
Rubin99 <rubinshrestha14@gmail.com>
2021-03-17 23:11:41 +0545

    Coding Challenge

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+## Coding Challenge
+![Coding Challenge](./CC.gif)
+
+## Task1
+![Task1 Screenshot](./Task1.png)
+
+## Task2
+![Task2 Screenshot](./Task2.png)
+
+
+## Task3
 ![Task3 Screenshot](./Task3.png)
--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,7 +75,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reset) {
+            mWordList.clear();
+            for (int i = 1; i < 21; i++){
+                mWordList.add("Word " + i);
+            }
+            Objects.requireNonNull(mRecyclerView.getAdapter()).notifyDataSetChanged();
             return true;
         }
 
